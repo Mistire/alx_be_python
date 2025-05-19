52bad2219389326a0cebc44279b53bfdc64ee359
Mistire <96515111+Mistire@users.noreply.github.com>
2025-05-19 17:01:54 +0300

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition_res = number1 + number2
+substraction_res = number1 - number2
+multiplication_res = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition_res}")
+print(f"Substraction of {number1} and {number2} is {substraction_res}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_res}")
+
