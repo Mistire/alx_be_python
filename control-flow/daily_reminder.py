97fed82349e308ab324b828d30a1f7149cdeab1a
Mistire <mdwithgod@gmail.com>
2025-05-27 19:05:43 +0300

    Create a personal daily reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,14 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+match priority:
+  case "high":
+    print(f"Reminder: '{task}' is a {priority} priority task", end=" ")
+  case "low":
+    print(f"Note: '{task}' is a {priority} priority task", end="")
+    
+if time_bound == "yes":
+  print("that requires immediate attention today!")
+else:
+  print(". Consider completing it when you have free time.")
