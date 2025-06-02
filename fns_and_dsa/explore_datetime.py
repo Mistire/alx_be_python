21c1b3143de52261976d6d8b7a6f85a1521a9e25
Mistire <mdwithgod@gmail.com>
2025-06-02 21:34:54 +0300

    fix: Update something so it could pass the alx checker

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-  current_date = datetime.datetime.now().replace(microsecond=0)
+  current_date = datetime.now().replace(microsecond=0)
   return current_date
 
 def calculate_future_date(no_of_days):
-  current_date = datetime.datetime.now().date()
-  return current_date + datetime.timedelta(days=no_of_days)
+  current_date = datetime.now().date()
+  return current_date + timedelta(days=no_of_days)
 
 print(f"Current date and time: {display_current_datetime()}")
 no_of_days = int(input("Enter the number of days to add to the current date: "))
