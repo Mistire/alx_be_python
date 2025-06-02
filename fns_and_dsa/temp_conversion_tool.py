ca062fbfd461b280e1f0b76409ed3226cefe67e8
Mistire <mdwithgod@gmail.com>
2025-06-03 00:00:05 +0300

    fix: Update everything so it could pass the alx checker..3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,16 +2,16 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-  return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+  return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-  return CELSIUS_TO_FAHRENHEIT_FACTOR + 32 * celsius
+  return (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
 
 temperature = float(input("Enter the temperature to convert: "))
 choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
 
 if choice == "f":
-  print(f"{temperature}°F is {convert_to_fahrenheit(temperature):.1f}°C")
+  print(f"{temperature}°F is {convert_to_celsius(temperature):.1f}°C")
 elif choice == "c":
   print(f"{temperature}°C is {convert_to_fahrenheit(temperature):.1f}°F")
 
