98d995fafe6beae2d2b1ce55b4ab2ed3c81d06f1
Mistire <mdwithgod@gmail.com>
2025-06-16 16:17:28 +0300

    fix: Update the attributes in PrintBook and EBook

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,17 +7,17 @@ class Book:
     return f"{__class__.__name__}: {self.title} by {self.author}"
 
 class EBook(Book):
-  def __init__(self, title, author, filesize):
+  def __init__(self, title, author, file_size):
     super().__init__(title, author)
-    self.filesize = int(filesize)
+    self.file_size = int(file_size)
 
   def __str__(self):
-    return f"{__class__.__name__}: {self.title} by {self.author}, File Size: {self.filesize}KB"
+    return f"{__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}KB"
   
 class PrintBook(Book):
-  def __init__(self, title, author, count):
+  def __init__(self, title, author, page_count):
     super().__init__(title, author)
-    self.page_count = int(count)
+    self.page_count = int(page_count)
 
   def __str__(self):
     return f"{__class__.__name__}: {self.title} by {self.author}, Page Count: {self.page_count}"
