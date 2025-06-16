28652f5f4b7eee0e1a2283ee55151fd39968b6f8
Mistire <mdwithgod@gmail.com>
2025-06-16 16:26:47 +0300

    Implement a simple class for demonstrating Polymorpism and Method Overriding

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+import math
+
+
+class Shape:
+  def __init__(self):
+    pass
+
+  def area(self):
+    raise NotImplementedError 
+
+class Rectangle(Shape):
+  def __init__(self, length, width):
+    self.length = length
+    self.width = width
+
+  def area(self):
+    return self.length * self.width
+  
+class Circle(Shape):
+  def __init__(self, radius):
+    self.radius = radius
+
+  def area(self):
+    return (self.radius**2) * math.pi
+
