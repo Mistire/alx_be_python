6461c97d03f19f9be3418c7f726bf40d1b5c4c0e
Mistire <mdwithgod@gmail.com>
2025-06-16 15:20:58 +0300

    fix: update the repr magic method input..3

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,7 +2,7 @@ class Book:
   def __init__(self, title, author, year):
     self.title = title
     self.author = author
-    self.year = int(year)
+    self.year = year
 
   def __del__(self):
     print(f"Deleting {self.title}")
@@ -10,5 +10,5 @@ class Book:
   def __str__(self):
     return f"{self.title} by {self.author}, published in {self.year}"
   def __repr__(self):
-    return f"Book('{self.title}', '{self.author}', '{self.year}')"
+    return f"Book('{self.title}', '{self.author}', {self.year})"
     
